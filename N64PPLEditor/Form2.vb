26c144f3ea0caac79b8798321636b89492204d33
Adrien <Adrien@DESKTOP-ADRIEN>
2020-01-17 00:35:58 +0100

    remove unused

--- N64PPLEditor/Form2.vb
+++ N64PPLEditor/Form2.vb
@@ -187,8 +187,4 @@ Public Class Form2
         fibStorage.writeAllData()
         Button7.BackColor = Color.LimeGreen
     End Sub
-
-    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
 End Class
